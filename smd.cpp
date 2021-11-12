84568cca41b038b25e12f8f3c1f602af5bf25f5c
ostanin406 <85750145+ostanin406@users.noreply.github.com>
2021-11-12 21:25:58 +0500

    Update smd.cpp

--- smd.cpp
+++ smd.cpp
@@ -2,21 +2,14 @@
 
 int CSmd::Load(const char* file)
 {
+#define floatok (float)atof(strtok(NULL, DELIM))
+#define vec2tok vec::vec2f{ floatok, floatok }
+#define vec3tok vec::vec3f{ floatok, floatok, floatok }
+
 	if (strcmp(strrchr(file, '.'), ".smd") != 0) return 0;
 
 	FILE* f = fopen(file, "rt");
 	if (!f) return 0;
-
-#define readline() \
-fgets(line, sizeof(line), f);\
-if (strlen(line) < 3) continue;\
-tok = strtok(line, DELIM);\
-end = strcmp(tok, "end");\
-if (end == 0) break
-
-#define floatok() (float)atof(strtok(NULL, DELIM))
-#define vec2tok() vec::vec2f{ floatok(), floatok() }
-#define vec3tok() vec::vec3f{ floatok(), floatok(), floatok() }
 
 	const char* DELIM = " \t\r\n";
 	char line[256];
@@ -25,28 +18,17 @@ if (end == 0) break
 
 	while (!feof(f))
 	{
-		fgets(line, sizeof(line), f);
-		if (strlen(line) < 3) continue;
-		tok = strtok(line, DELIM);
-
-		if (strcmp(tok, "version") == 0)
-		{
-			printf("[SMD] version %s\n", strtok(NULL, DELIM));
-			continue;
-		}
+		if ((tok = ReadLine(line, sizeof(line), f)) == 0) continue;
 
 		if (strcmp(tok, "nodes") == 0)
 		{
-			printf("[SMD] nodes\n");
 			while (!feof(f))
 			{
-				readline();
+				if ((tok = ReadLine(line, sizeof(line), f)) == 0) continue;
+				if ((end = strcmp(tok, "end")) == 0) break;
 
-				nodes.name = strtok(NULL, "\t\r\n\"");
-				if (nodes.name.empty()) return 0;
-				nodes.parent = atoi(strtok(NULL, DELIM));
-
-				data.nodes.push_back(nodes);
+				tok = strtok(NULL, "\""); // Bone name
+				data.nodes_parent.push_back(atoi(strtok(NULL, DELIM)));
 			}
 			if (end != 0) return 0;
 			continue;
@@ -54,22 +36,24 @@ if (end == 0) break
 
 		if (strcmp(tok, "skeleton") == 0)
 		{
-			printf("[SMD] skeleton\n");
+			skeleton_t skeleton;
 			while (!feof(f))
 			{
-				readline();
+				if ((tok = ReadLine(line, sizeof(line), f)) == 0) continue;
+				if ((end = strcmp(tok, "end")) == 0) break;
 
 				if (strcmp(tok, "time") == 0)
 				{
-					skeleton.time = atoi(strtok(NULL, DELIM));
+					tok = strtok(NULL, DELIM); // Time count
 
-					for (size_t i = 0; i < data.nodes.size(); i++)
+					for (size_t i = 0; i < data.nodes_parent.size(); i++)
 					{
 						if (feof(f)) return 0;
-						readline();
+						if ((tok = ReadLine(line, sizeof(line), f)) == 0) continue;
+						if ((end = strcmp(tok, "end")) == 0) break;
 
-						skeleton.pos.push_back(vec3tok());
-						skeleton.rot.push_back(vec3tok());
+						skeleton.pos.push_back(vec3tok);
+						skeleton.rot.push_back(vec3tok);
 					}
 				}
 				data.skeleton.push_back(skeleton);
@@ -80,24 +64,23 @@ if (end == 0) break
 
 		if (strcmp(tok, "triangles") == 0)
 		{
-			printf("[SMD] triangles\n");
+			triangles_t triangles;
 			while (!feof(f))
 			{
-				readline();
+				if ((tok = ReadLine(line, sizeof(line), f)) == 0) continue;
+				if ((end = strcmp(tok, "end")) == 0) break;
 
-				triangles.name = tok;
-				if (triangles.name.empty()) return 0;
+				triangles.matid = MaterialID(tok);
 
 				for (int i = 0; i < 3; i++)
 				{
 					if (feof(f)) return 0;
-					fgets(line, sizeof(line), f);
-					if (strlen(line) < 3) continue;
+					if ((tok = ReadLine(line, sizeof(line), f)) == 0) continue;
 
-					triangles.bone[i] = atoi(strtok(line, DELIM));
-					triangles.pos[i] = vec3tok();
-					triangles.norm[i] = vec3tok();
-					triangles.uv[i] = vec2tok();
+					triangles.bone[i] = atoi(tok);
+					triangles.pos[i] = vec3tok;
+					triangles.norm[i] = vec3tok;
+					triangles.uv[i] = vec2tok;
 				}
 				data.triangles.push_back(triangles);
 			}
@@ -106,13 +89,12 @@ if (end == 0) break
 		}
 	}
 
+	fclose(f);
+	return 1;
+
 #undef floatok
 #undef vec2tok
 #undef vec3tok
-#undef readline
-
-	fclose(f);
-	return 1;
 }
 
 int CSmd::Save(const char* file)
@@ -125,15 +107,15 @@ int CSmd::Save(const char* file)
 	fputs("version 1\n", f);
 
 	fputs("nodes\n", f);
-	for (size_t i = 0; i < data.nodes.size(); i++)
-		fprintf(f, "%i \"%s\" %i\n", i, data.nodes[i].name.c_str(), data.nodes[i].parent);
+	for (size_t i = 0; i < data.nodes_parent.size(); i++)
+		fprintf(f, "%i \"bone%i\" %i\n", i, i, data.nodes_parent[i]);
 	fputs("end\n", f);
 
 	fputs("skeleton\n", f);
 	for (size_t i = 0; i < data.skeleton.size(); i++)
 	{
-		fprintf(f, "time %i\n", data.skeleton[i].time);
-		for (size_t j = 0; j < data.nodes.size(); j++)
+		fprintf(f, "time %i\n", i);
+		for (size_t j = 0; j < data.nodes_parent.size(); j++)
 		{
 			fprintf(f, "%i %f %f %f %f %f %f\n", j,
 				data.skeleton[i].pos[j].x,
@@ -146,26 +128,48 @@ int CSmd::Save(const char* file)
 	}
 	fputs("end\n", f);
 
-	fputs("triangles\n", f);
-	for (size_t i = 0; i < data.triangles.size(); i++)
+	if (data.triangles.size() != 0)
 	{
-		fprintf(f, "%s\n", data.triangles[i].name.c_str());
-		for (int j = 0; j < 3; j++)
+		fputs("triangles\n", f);
+		for (size_t i = 0; i < data.triangles.size(); i++)
 		{
-			fprintf(f, "%i %f %f %f %f %f %f %f %f\n",
-				data.triangles[i].bone[j],
-				data.triangles[i].pos[j].x,
-				data.triangles[i].pos[j].y,
-				data.triangles[i].pos[j].z,
-				data.triangles[i].norm[j].x,
-				data.triangles[i].norm[j].y,
-				data.triangles[i].norm[j].z,
-				data.triangles[i].uv[j].x,
-				data.triangles[i].uv[j].y);
+			fprintf(f, "%s\n", data.material[data.triangles[i].matid].c_str());
+			for (int j = 0; j < 3; j++)
+			{
+				fprintf(f, "%i %f %f %f %f %f %f %f %f\n",
+					data.triangles[i].bone[j],
+					data.triangles[i].pos[j].x,
+					data.triangles[i].pos[j].y,
+					data.triangles[i].pos[j].z,
+					data.triangles[i].norm[j].x,
+					data.triangles[i].norm[j].y,
+					data.triangles[i].norm[j].z,
+					data.triangles[i].uv[j].x,
+					data.triangles[i].uv[j].y);
+			}
 		}
+		fputs("end\n", f);
 	}
-	fputs("end\n", f);
 
 	fclose(f);
 	return 1;
+}
+
+char* CSmd::ReadLine(char* line, int count, FILE* file)
+{
+	fgets(line, count, file);
+	if (strlen(line) < 3) return 0;
+	return strtok(line, " \t\r\n");
+}
+
+int CSmd::MaterialID(const char* name)
+{
+	int countmat = data.material.size();
+	for (int i = 0; i < countmat; i++)
+	{
+		if (strcmp(name, data.material[i].c_str()) == 0)
+			return i;
+	}
+	data.material.push_back(name);
+	return countmat;
 }
