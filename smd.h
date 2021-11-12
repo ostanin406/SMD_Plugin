2e185f9d545b8f5291dfb78e220c870291f22d8f
ostanin406 <85750145+ostanin406@users.noreply.github.com>
2021-11-12 21:27:08 +0500

    Update smd.h

--- smd.h
+++ smd.h
@@ -11,36 +11,25 @@ public:
 	int Save(const char* file);
 
 private:
-	struct nodes_t
-	{
-		std::string name;
-		int parent = 0;
-	};
+	char* ReadLine(char* line, int count, FILE* file);
+	int MaterialID(const char* name);
 
-	struct skeleton_t
-	{
-		int time = 0;
-		std::vector <vec::vec3f> pos, rot;
-	};
+	struct skeleton_t { std::vector <vec::vec3f> pos, rot; };
 
 	struct triangles_t
 	{
-		std::string name;
-		int bone[3];
+		int bone[3], matid;
 		vec::vec3f pos[3], norm[3];
 		vec::vec2f uv[3];
 	};
 
 	struct data_t
 	{
-		std::vector <nodes_t> nodes;
+		std::vector <int> nodes_parent;
 		std::vector <skeleton_t> skeleton;
 		std::vector <triangles_t> triangles;
+		std::vector <std::string> material;
 	};
-
-	nodes_t nodes;
-	skeleton_t skeleton;
-	triangles_t triangles;
 
 public:
 	data_t data;
